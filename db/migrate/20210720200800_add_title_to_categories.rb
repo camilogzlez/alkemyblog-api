a5ea5b961ba32ef76ebd667f3988a7001a3ef6a0
camilo gonzalez <camilogzlez@gmail.com>
2021-07-20 17:10:31 -0300

    db created

--- db/migrate/20210720200800_add_title_to_categories.rb
+++ db/migrate/20210720200800_add_title_to_categories.rb
@@ -1,4 +1,5 @@
 class AddTitleToCategories < ActiveRecord::Migration[6.1]
   def change
+    add_column :categories, :title, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_125006) do
+ActiveRecord::Schema.define(version: 2021_07_20_200800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_07_20_125006) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
